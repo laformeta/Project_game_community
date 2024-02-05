2e0ffe5698aba481e777dfd1e9dc0d4da720ac97
kims_laformeta <laformeta@naver.com>
2024-02-05 18:28:21 +0900

    gitignore yml파일 등록후 개발시작

---
+++ src/main/java/com/cogamers/CoGamersApplication.java
@@ -0,0 +1,13 @@
+package com.cogamers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoGamersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoGamersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cogamers/CoGamersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogamers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoGamersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
