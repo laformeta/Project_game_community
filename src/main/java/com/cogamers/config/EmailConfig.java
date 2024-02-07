04efce69efe718626aaa94cbf7b19e2e07cadb72
kims_laformeta <laformeta@naver.com>
2024-02-07 18:37:59 +0900

    email 인증기 구현 중간과정

--- src/main/java/com/cogamers/config/EmailConfig.java
+++ src/main/java/com/cogamers/config/EmailConfig.java
@@ -1,13 +1,17 @@
 package com.cogamers.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import java.util.Properties;
 
-// 나중에 확인시 라이브러리 임포트 확인 (lombok vs Springframework)
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class EmailConfig {
-	@Value("${spring.mail.host}")
+
+    @Value("${spring.mail.host}")
     private String host;
 
     @Value("${spring.mail.port}")
@@ -36,4 +40,29 @@ public class EmailConfig {
 
     @Value("${spring.mail.properties.mail.smtp.writetimeout}")
     private int writeTimeout;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setDefaultEncoding("UTF-8");
+        mailSender.setJavaMailProperties(getMailProperties());
+
+        return mailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", auth);
+        properties.put("mail.smtp.starttls.enable", starttlsEnable);
+        properties.put("mail.smtp.starttls.required", starttlsRequired);
+        properties.put("mail.smtp.connectiontimeout", connectionTimeout);
+        properties.put("mail.smtp.timeout", timeout);
+        properties.put("mail.smtp.writetimeout", writeTimeout);
+
+        return properties;
+    }
 }
