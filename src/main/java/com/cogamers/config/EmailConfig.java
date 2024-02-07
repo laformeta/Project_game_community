1e94511ac69450e13a67eb6d3ea95e7b46e77191
kims_laformeta <laformeta@naver.com>
2024-02-07 17:26:11 +0900

    laformeta/Project_game_community#4 email 인증기 테스트시 라이브러리 체크(롬복vs스프링프레임워크)

---
+++ src/main/java/com/cogamers/config/EmailConfig.java
@@ -0,0 +1,39 @@
+package com.cogamers.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+// 나중에 확인시 라이브러리 임포트 확인 (lombok vs Springframework)
+
+@Configuration
+public class EmailConfig {
+	@Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean starttlsEnable;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.required}")
+    private boolean starttlsRequired;
+
+    @Value("${spring.mail.properties.mail.smtp.connectiontimeout}")
+    private int connectionTimeout;
+
+    @Value("${spring.mail.properties.mail.smtp.timeout}")
+    private int timeout;
+
+    @Value("${spring.mail.properties.mail.smtp.writetimeout}")
+    private int writeTimeout;
+}
